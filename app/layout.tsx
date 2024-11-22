0a4840303def8835774cb90f9028ac939c71a17f
Fuma <leonardfuma26@gmail.com>
2024-11-22 17:30:11 +0200

    deploy 06

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import StarsCanvas from "@/components/main/StarBackground";
 import Navbar from "@/components/main/Navbar";
 
 const geistSans = localFont({
