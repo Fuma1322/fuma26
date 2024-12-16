ffd0a090137ab13b9cb7d040fdca922191146f1c
Fuma <leonardfuma26@gmail.com>
2024-12-16 17:52:13 +0200

    Stars Background

--- components/main/Stars.tsx
+++ components/main/Stars.tsx
@@ -5,41 +5,46 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { Points, PointMaterial } from "@react-three/drei";
 import * as random from "maath/random/dist/maath-random.esm";
 
-const StarBackground = (props: any) => {
-  const ref: any = useRef();
+interface StarBackgroundProps {
+  [key: string]: any;
+}
+
+const StarBackground: React.FC<StarBackgroundProps> = (props) => {
+  const ref = useRef<THREE.Group>(null);
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
 
   useFrame((state, delta) => {
-    ref.current.rotation.x -= delta/10;
-    ref.current.rotation.y -= delta/15;
-  })
-
+    if (ref.current) {
+      ref.current.rotation.x -= delta / 10;
+      ref.current.rotation.y -= delta / 15;
+    }
+  });
 
   return (
-    <group rotation={[0,0, Math.PI / 4]}>
-        <Points
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points
         ref={ref}
         positions={sphere}
         stride={3}
         frustumCulled
         {...props}
-        >
-            <PointMaterial
-                transparent
-                color="$fff"
-                size={0.002}
-                sizeAttenuation={true}
-                dethWrite={false}
-            />
-        </Points>
+      >
+        <PointMaterial
+          transparent
+          color="#fff"
+          size={0.002}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
     </group>
-  )
+  );
 };
 
-const StarsCanvas = () => (
-  <div className="w-full h-auto fixed inset-0 z-[10]">
+const StarsCanvas: React.FC = () => (
+  <div className="w-full h-auto fixed inset-0 z-[-1]">
     <Canvas camera={{ position: [0, 0, 1] }}>
       <Suspense fallback={null}>
         <StarBackground />
