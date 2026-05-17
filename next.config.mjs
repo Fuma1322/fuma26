a2d13bfde7523e3b785367703f4b1f4e220695f8
Tankiso Fuma <146758442+Fuma1322@users.noreply.github.com>
2026-05-17 16:53:26 -0400

    Update image domains in next.config.mjs
    Added 'ufs.sh' to the list of allowed image domains.

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: ['tailwindui.com','flowbite.s3.amazonaws.com','assets.aceternity.com',"utfs.io", 'images.unsplash.com', 'pbs.twimg.com'],
-    },
+  images: {
+    domains: [
+      "tailwindui.com",
+      "flowbite.s3.amazonaws.com",
+      "assets.aceternity.com",
+      "utfs.io",
+      "ufs.sh",
+      "images.unsplash.com",
+      "pbs.twimg.com",
+    ],
+  },
 };
-
 
 export default nextConfig;
